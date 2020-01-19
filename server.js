0615cea392da838a1fcac1aa78733418f11310c0
I531549 <victor.varian@sap.com>
2020-01-19 20:43:50 +0800

    Configure heroku port

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.listen(5000, process.env.IP);
+app.listen(process.env.PORT || 5000);
 console.log("Server running at localhost:5000");
